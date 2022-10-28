7cf3ed0998f16e9fb513250ea3760b054116a1ac
홍준성 <ghdwnstjd13@gmail.com>
2022-10-28 18:36:42 +0900

    이터레이터 패턴

---
+++ src/main/kotlin/iteratorpattern/Car.kt
@@ -0,0 +1,30 @@
+package iteratorpattern
+
+data class Car(val brand: String)
+
+class CarIterable(val cars: List<Car> = listOf()) : Iterable<Car> {
+
+    override fun iterator(): Iterator<Car> = CarIterator(cars)
+
+}
+
+class CarIterator(
+    val cars: List<Car> = listOf(),
+    var index: Int = 0,
+) : Iterator<Car> {
+
+    override fun hasNext(): Boolean = cars.size > index
+
+    override fun next(): Car = cars[index++]
+}
+
+fun main() {
+
+    val carIterable = CarIterable(listOf(Car("람보르기니"), Car("페라리")))
+
+    val iterator = carIterable.iterator()
+
+    while (iterator.hasNext()) {
+        println("브랜드: ${iterator.next()}")
+    }
+}

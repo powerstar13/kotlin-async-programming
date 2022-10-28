174da2349bb4eb32aa4181ca2dfdb885434984ec
홍준성 <ghdwnstjd13@gmail.com>
2022-10-28 13:24:12 +0900

    비동기 프로그래밍 > Thread

---
+++ src/main/kotlin/thread/Thread.kt
@@ -0,0 +1,13 @@
+package thread
+
+fun main() {
+
+    for (i in 0..5) {
+
+        val thread = Thread {
+            println("current-thread-name: ${Thread.currentThread().name}")
+        }
+        thread.start()
+    }
+    println("current-thread-name: ${Thread.currentThread().name}")
+}
